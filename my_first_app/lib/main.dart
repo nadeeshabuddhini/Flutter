e83b795d66f7e47d171cb68cad74bd60ddb7d3b2
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-11-20 22:29:31 +0530

    flutter
    login page

---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/LoginPage.dart';
+void main()=> runApp(new MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'student attendance',
+      theme: new ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home: new LoginPage(),
+    );
+  }
+
+}
+
