ad7074895a59371340fcaeed7b6bf3d193ee8082
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-12-23 01:04:41 +0530

    Add files via upload

--- attendance_app/lib/Home.dart
+++ attendance_app/lib/Home.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
- class Home extends StatelessWidget{
+class Home extends StatelessWidget{
+  List<Container>AdminHome=new List();
+  var character=[
+    {"name":"Student","picture":"logo.png"},
+    {"name":"Student","picture":"logo.png"},
+    {"name":"Student","picture":"logo.png"},
+    {"name":"Student","picture":"logo.png"},
+    {"name":"Student","picture":"logo.png"},
+  ];
+  _bulletlist()async{
+    for(var i=0; i<character.length; i++){
+      final characters=character[i];
+      final String picture=characters["picture"];
+     AdminHome.add(
+       new Container(
+         padding: new EdgeInsets.all(10.0),
+         child: new Card(child: new Column(
+           children: <Widget>[
+             new Image.asset("images/$picture",fit: BoxFit.cover,),
+             new Text(characters['name'],style: new TextStyle(fontSize: 18.0),)
+           ],
+         ),),
+       )
+     );
+
+
+    }
+  }
+   
+
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-
+      appBar:AppBar(
+        title: Text('Home'
+        ),
+      ),
+      body: new GridView.count(
+          crossAxisCount: 2,
+      children: AdminHome,
       ),
     );
   }
 
- }
+}
