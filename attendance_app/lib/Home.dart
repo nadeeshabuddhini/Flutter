60aa022f1271ebc8b1b6e49efba4085ce80b3253
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-12-23 23:02:12 +0530

    Add files via upload

--- attendance_app/lib/Home.dart
+++ attendance_app/lib/Home.dart
@@ -1,49 +1,42 @@
 import 'package:flutter/material.dart';
-
 class Home extends StatelessWidget{
-  List<Container>AdminHome=new List();
-  var character=[
-    {"name":"Student","picture":"logo.png"},
-    {"name":"Student","picture":"logo.png"},
-    {"name":"Student","picture":"logo.png"},
-    {"name":"Student","picture":"logo.png"},
-    {"name":"Student","picture":"logo.png"},
-  ];
-  _bulletlist()async{
-    for(var i=0; i<character.length; i++){
-      final characters=character[i];
-      final String picture=characters["picture"];
-     AdminHome.add(
-       new Container(
-         padding: new EdgeInsets.all(10.0),
-         child: new Card(child: new Column(
-           children: <Widget>[
-             new Image.asset("images/$picture",fit: BoxFit.cover,),
-             new Text(characters['name'],style: new TextStyle(fontSize: 18.0),)
-           ],
-         ),),
-       )
-     );
-
-
-    }
-  }
-   
-
-
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
+    // TODO: implement build
+    return new Scaffold(
       appBar:AppBar(
         title: Text('Home'
         ),
+
       ),
       body: new GridView.count(
-          crossAxisCount: 2,
-      children: AdminHome,
+        crossAxisCount: 2,
+        children: <Widget>[
+          new Container(
+            child: new Card(
+              elevation: 10.0,
+              child: new Column(
+                children: <Widget>[
+                 new Image.asset("images/logo.png",
+                 height: 100.0,
+                 width: 100.0,
+                 fit: BoxFit.cover,),
+                  new SizedBox(height:5.0,),
+                  new Text("Student",
+                  style:TextStyle(
+                    fontSize:20.0,
+                    color:Colors.black
+                  ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
 
 }
+
+
