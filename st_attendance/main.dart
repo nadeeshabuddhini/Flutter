4caa881738b0b71e06f847d9f7225f5267c06fb5
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2019-12-24 19:47:21 +0530

    Add files via upload

---
+++ st_attendance/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:st_attendance/Login.dart';
+import 'package:st_attendance/Home.dart';
+
+void main()=> runApp(new MyApp());
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'student attendance',
+      routes: <String,WidgetBuilder>{
+        '/Login': (BuildContext context) => new Login(),
+        '/Home': (BuildContext context) => new  Home(),
+      },
+      theme: new ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home: new Login(),
+    );
+  }
+
+}
