d05f3d6988589e3a5fdb7229a9239ceefbd0a85c
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2020-01-14 11:27:59 +0530

    Add files via upload

---
+++ st_attendance/lib/student_detail.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:student_attendance_app/AddTask.dart';
+
+
+class student_detail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      floatingActionButton:new FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(new MaterialPageRoute(
+              builder:(BuildContext context)=>new AddTask())
+          );
+
+        },
+    child:Icon(Icons.add),
+        backgroundColor:Colors.lightBlueAccent,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: new BottomAppBar(
+        elevation: 30.0,
+        color: Colors.blue,
+        child: ButtonBar(
+          children: <Widget>[],
+        ),
+      ),
+      appBar:AppBar(
+        title: Text('Student Details'
+        ),
+
+      ),
+    );
+
+  }
+}
