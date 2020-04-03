e12d8e753dc3dad20049181cab3fd85e92e1d849
N_Buddhini <39161490+nadeeshabuddhini@users.noreply.github.com>
2020-04-03 23:04:49 +0530

    Update Login.dart

--- st_attendance/lib/Login.dart
+++ st_attendance/lib/Login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:student_attendance/Home.dart';
+import 'package:st_attendance/Home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
